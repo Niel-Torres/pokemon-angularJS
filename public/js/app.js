6e05df78142dc6611ecf127cc6fb91bf1ad466a6
ntorresm <odon.niel.torres.mariluz@everis.com>
2020-11-07 23:31:22 +0100

    feature: agregamos boostrap

--- public/js/app.js
+++ public/js/app.js
@@ -12,15 +12,14 @@
     //Controlador
     app.controller('PokemonController', function () {
       this.pokemon = {
-        id: 001,
-        name: 'Bullbasur',
-        species: 'Seed Pokemon',
-        type: ['Grass', 'Poison'],
-        height: "2'4",
-        weight: '15.2 lbs',
-        abilities: ['Overgrow', 'Chlorophyll']
-
-      };  
+        id: "001",
+        name: "Bulbasaur",
+        species: "Seed Pokémon",
+        type: [ "Grass", "Poison" ],
+        height: "2′4″ (0.71m)",
+        weight: "15.2 lbs (6.9 kg)",
+        abilities: [ "Overgrow", "Chlorophyll"]
+      }; 
     });
 
 })();
