d1906e651f4c21f1c063bd28bb611618b8a2375a
ntorresm <odon.niel.torres.mariluz@everis.com>
2020-12-09 00:44:31 +0100

    feature: Crear modulos para directivas y filtros

---
+++ public/js/directives.js
@@ -0,0 +1,69 @@
+(function() {
+    angular.module('pokedex.directives', [])
+        .directive('pokemonData', function() {
+            return {
+            restrict: 'E',
+            templateUrl: 'partials/pokemon-data.html'
+            }
+        })
+  
+        .directive('pokemonName', function() {
+            return {
+            restrict: 'E',
+            templateUrl: 'partials/pokemon-name.html'
+            }
+        })
+    
+        .directive('pokemonImage', function() {
+            return {
+            restrict: 'E',
+            templateUrl: 'partials/pokemon-image.html'
+            }
+        })
+    
+        .directive('pokemonStats', function() {
+            return {
+            restrict: 'E',
+            templateUrl: 'partials/pokemon-stats.html'
+            }
+        })
+    
+        .directive('pokemonEvolution', function() {
+            return {
+            restrict: 'E',
+            templateUrl: 'partials/pokemon-evolution.html'
+            }
+        })
+    
+        .directive('pokemonComments', function() {
+            return {
+            restrict: 'E',
+            templateUrl: 'partials/pokemon-comments.html',
+            controller: function () {
+                this.comments = [];
+                this.comment = {};
+                this.show = true;
+    
+                this.toggle = function (){
+                this.show = !this.show;
+                }
+    
+                this.anonymousChanged = function () {
+                if(this.comment.anonymous){
+                    this.comment.email = ""; 
+                }
+                };
+    
+                this.addComment = function () {
+                this.comments.push(this.comment);
+                this.comment.date = Date.now();
+                this.comment = {};
+                };  
+            },
+            controllerAs: 'cmtsCtrl'
+            }
+        });
+
+})();
+
+
---
+++ public/js/filters.js
@@ -0,0 +1,9 @@
+(function() {
+    angular.module('pokedex.filters',[])
+        .filter('imageify', function() {
+            return function (input) {
+            var url = "img/pokemons/" + input.toLowerCase() + ".jpg";
+            return url;
+            }
+        })
+})();
