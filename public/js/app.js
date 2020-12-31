c109fbf8b3394ac90d47cf47cf7cf5c8ce08c6ab
Niel Torres <niel.torres08@gmail.com>
2020-12-31 02:20:47 +0100

    feature: agregamos ngRoute y rutas básicas para cargar el pokemón

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,25 @@
 /**
  * Declaramos una función IIFE - Es decir una función que se llama a si misma "()"
  * IIFE: Expresión de función ejecutada inmediatamente
- * (IIFE por su sigla en inglés) son funciones que se ejecutan tan pronto como se definen. 
- */ 
+ * (IIFE por su sigla en inglés) son funciones que se ejecutan tan pronto como se definen.
+ */
 (function () {
-    //Modulo
-    var app = angular.module('pokedex', [
-      'pokedex.controllers',
-      'pokedex.directives',
-      'pokedex.filters'
-    ]);
+  //Modulo
+  var app = angular.module("pokedex", [
+    "ngRoute",
+    "pokedex.controllers",
+    "pokedex.directives",
+    "pokedex.filters",
+  ]);
 
+  app.config([
+    "$routeProvider",
+    function ($routeProvider) {
+      $routeProvider.when("/", {
+        templateUrl: "views/pokemon.html",
+        controller: "PokemonController",
+        controllerAs: "pkmCtrl",
+      });
+    },
+  ]);
 })();
