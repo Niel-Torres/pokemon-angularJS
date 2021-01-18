ec9f11e8157fbeaddc263dce48e3c497dc948172
Niel Torres <niel.torres08@gmail.com>
2021-01-18 20:10:21 +0100

    feature: creamos vista pokedex

--- public/js/app.js
+++ public/js/app.js
@@ -14,14 +14,19 @@
     "pokedex.filters",
   ]);
 
-  app.config([
-    "$routeProvider",
-    function ($routeProvider) {
-      $routeProvider.when("/", {
-        templateUrl: "views/pokemon.html",
-        controller: "PokemonController",
-        controllerAs: "pkmCtrl",
-      });
+  app.config(["$routeProvider", function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/pokedex.html'
+        })
+        .when("/pokemon/:id", {
+          templateUrl: "views/pokemon.html",
+          controller: "PokemonController",
+          controllerAs: "pkmCtrl",
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
     },
   ]);
 })();
