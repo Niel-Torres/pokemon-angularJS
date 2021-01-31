1b645eeb4a0b9b9209e171d83ac234f5427d0b12
Niel Torres <niel.torres08@gmail.com>
2021-01-31 17:48:47 +0100

    feature: Add filter to normalize images

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,9 +1,28 @@
 (function() {
     angular.module('pokedex.filters',[])
+        .filter('normalize', function(){
+            return function(input) {
+                input = input
+                    .replace('♀', 'f')
+                    .replace('♂', 'm')
+                    .replace(/\W+/g, '')
+                return input.toLowerCase();
+            }
+        })
+
+        .filter('imageify', ['$filter', function($filter) {
+            return function (input) {
+            var url = "img/pokemons/" + $filter('normalize')(input) + ".jpg";
+            return url;
+            }
+        }])
+
+        /* opcion OLD - filter imagify
         .filter('imageify', function() {
             return function (input) {
             var url = "img/pokemons/" + input.toLowerCase() + ".jpg";
             return url;
             }
         })
+        */
 })();
