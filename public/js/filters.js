8380e7444be4f0aa1ca3c1ead145012b620b6802
Niel Torres <niel.torres08@gmail.com>
2021-03-16 23:41:15 +0100

    feature: Añado botón follow github nieltorres y corrijo fix para librerias de angular

--- public/js/filters.js
+++ public/js/filters.js
@@ -14,7 +14,7 @@
 
         .filter('imageify', ['$filter', function($filter) {
             return function (input) {
-            var url = "img/pokemons/" + $filter('normalize')(input) + ".jpg";
+            var url = "./img/pokemons/" + $filter('normalize')(input) + ".jpg";
             return url;
             }
         }])
